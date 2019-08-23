3df1d81b87f818638b160fe9de756d9c32d01737
Randy <randy.zhu.zhiwei@gmail.com>
2019-08-23 16:33:27 +1000

    Commit
    Commit

---
+++ Assets/move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed = 6.0F;
+    public bool canMove = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            Vector3 moveDirection = Vector3.forward;
+            transform.position += moveDirection * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/timer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timer : MonoBehaviour
+{
+    Material m_Material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Material = GetComponent<Renderer>().material;
+        StartCoroutine("TrafficLights");
+        m_Material.color = Color.red;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_Material.color == Color.red)
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, 5f);
+            int i = 0;
+            if (hitColliders.Length > 1)
+            {
+                while (i < hitColliders.Length)
+                {
+                    if(hitColliders[i].gameObject.tag=="cars")
+                    {
+                        hitColliders[i].gameObject.GetComponent<move>().canMove = false;
+                    }
+                    i++;
+                }
+            }
+        }
+        else
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, 5f);
+            int i = 0;
+            if (hitColliders.Length > 1)
+            {
+                while (i < hitColliders.Length)
+                {
+                    if (hitColliders[i].gameObject.tag == "cars")
+                    {
+                        hitColliders[i].gameObject.GetComponent<move>().canMove = true;
+                    }
+                    i++;
+                }
+            }
+        }
+
+    }
+     IEnumerator TrafficLights()
+    {
+        while (true)
+        {
+            if (m_Material.color == Color.red)
+            {
+                m_Material.color = Color.green;
+            }else
+            {
+                m_Material.color = Color.red;
+            }
+            yield return new WaitForSeconds(5f);
+        }
+    }
+}
